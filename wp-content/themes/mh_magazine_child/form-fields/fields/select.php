075899262e393658d685eef91d16cfe25eb0d9d6
Jason Nelson <jason.nelson@salesforce.com>
2018-10-26 15:23:39 -0400

    changes made during hackathon

---
+++ wp-content/themes/mh_magazine_child/form-fields/fields/select.php
@@ -0,0 +1,13 @@
+<label class="customizer-field" id="<?php echo $input_name ?>"
+ for="<?php echo $input_name ?>" <?php echo(isset($field_info['hidden']) && $field_info['hidden']) ? 'style="display:none"' : '' ?>>
+ 
+ <?php echo $input_description ?><?php echo $required_label ?>
+ 
+<?php foreach($field_info['select_options'] as $option_value => $option_name) : ?>
+	<?php if($option_value == 'other') : ?>
+ 	<div class="select"><input type="select" id="<?php echo $input_name . '_' . $option_value ?>" name="<?php echo $input_name ?>" value="<?php echo $option_value ?>"<?php echo $required_input ?>><label for="<?php echo $input_name . '_' . $option_value ?>"><?php echo $option_name ?> <input type="text" name="<?php echo $input_name ?>" style="display:none; margin:0 25px"></label></div>
+	<?php else : ?>
+ 	<div class="select"><input type="select" id="<?php echo $input_name . '_' . $option_value ?>" name="<?php echo $input_name ?>" value="<?php echo $option_value ?>"<?php echo $required_input ?>><label for="<?php echo $input_name . '_' . $option_value ?>"><?php echo $option_name ?></label></div>
+	<?php endif ?>
+<?php endforeach ?>
+</label>
